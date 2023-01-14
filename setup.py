3c2500a38b9699de74424cd8c57bb7470a3637c8
Sheng Zhong <zhsh@umich.edu>
2023-01-14 18:15:28 -0500

    Version 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multidim-indexing',
-    version='0.7.0',
+    version='0.8.0',
     packages=find_packages(),
     url='',
     license='',
